94e3fbcc45612761a30b6177483f4ad1dcde48c7
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-05-08 18:00:03 +0700

    Add files via upload

---
+++ precache-manifest.8a8347464da85e2cbe69301929925781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "decbd3b9b86a7a81d45e",
+    "url": "/static/js/main.444f7ea1.chunk.js"
+  },
+  {
+    "revision": "6a85151cca8c991b8fd3",
+    "url": "/static/js/2.e82a78c9.chunk.js"
+  },
+  {
+    "revision": "decbd3b9b86a7a81d45e",
+    "url": "/static/css/main.bc76ddea.chunk.css"
+  },
+  {
+    "revision": "3880a53f217f64eb72247bdd545bcfb7",
+    "url": "/index.html"
+  }
+];
